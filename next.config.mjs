0c2abced990f1f0cd2ff956d0636870624131d00
grandmasterkd <thewqmps@gmail.com>
2025-12-18 22:56:59 +0000

    test: next config for stripe paths

--- next.config.mjs
+++ next.config.mjs
@@ -34,6 +34,15 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://m.stripe.com; connect-src 'self' https://api.stripe.com https://m.stripe.network https://m.stripe.com https://stripe.com; frame-src 'self' https://js.stripe.com https://m.stripe.com https://hooks.stripe.com; img-src 'self' data: https://*.stripe.com https://stripe.com;",
+          },
+        ],
+      },
+      {
         source: '/profiles/:path*',
         headers: [
           {
